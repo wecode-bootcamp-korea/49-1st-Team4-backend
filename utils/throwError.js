a07161e65a01c1899bd2d0f333bb961570e537af
JISUlicious <ljshong@hanmail.net>
2023-09-11 21:44:33 +0900

    FIX: removed console.log

--- utils/throwError.js
+++ utils/throwError.js
@@ -1,8 +1,6 @@
 const throwError = (statusCode, message) => {
-  console.log("creating new error");
   const error = new Error(message);
   error.status = statusCode;
-  console.log("new error", error);
   throw error;
 };
 
