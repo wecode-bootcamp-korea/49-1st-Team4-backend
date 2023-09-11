6145a864b2b26050af6aeb7c8e0ffc5b261e9fdc
JISUlicious <ljshong@hanmail.net>
2023-09-11 18:43:33 +0900

    CHANGE: move loginc to userService

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,24 +1,6 @@
 const {throwError} = require("../utils/throwError");
 
 const signUp = async (req, res) => {
-  // req body validation
-  // is not-null valus exist
-  const { email, password, nickname, phoneNumber, birthday, profileImage } =
-    req.body;
-  userService.checkEmptyValues(email, password, nickname);
-
-  // is email format correct
-  userService.checkEmailValidity(email);
-
-  // is password format correct
-  userService.checkPasswordValidity(password);
-
-  // is email not exist on db
-  
-
-  // password hashing
-
-  // db save
-
-  // response
+  userService.signUp(req.body);
+  res.status(201).json({message: "userCreated"});
 };
