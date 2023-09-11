0c8b4ddc6b8d4f448b77939fd7e1db23ed07a51d
JunwooPark <wnsdn1994@gmail.com>
2023-09-11 15:37:19 +0900

    UPDATE : dbmate_migration

---
+++ db/migrations/20230911053025_create_threads_table.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+CREATE TABLE threads(
+        id INT NOT NULL AUTO_INCREMENT,
+        content VARCHAR(3000) NULL,
+        user_id INT NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+);
+-- migrate:down
+DROP TABLE threads;
