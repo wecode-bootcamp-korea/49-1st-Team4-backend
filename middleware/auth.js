d878a33bb0c71ed7895747e004fe20861d233cf6
JISU <52498558+JISUlicious@users.noreply.github.com>
2023-09-13 18:37:14 +0900

    Update auth.js removed unused variable

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ const validateToken = async (req, res, next) => {
     const token = req.headers.authorization;
     const { id } = jwt.verify(token, AUTH_TOKEN_SALT); // 암호화된 토큰을 복호화 합니다.
 
-    const foundUser = await UserService.findUser(id);
+    await UserService.findUser(id);
 
     req.body.user_id = id; // request 객체에 새로운 키값에 찾아진 유저의 정보를 할당하고
     next(); // next() 함수로 다음 미들웨어로 맥락(context)를 연결합니다.
