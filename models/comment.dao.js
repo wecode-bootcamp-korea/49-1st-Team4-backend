ddaa98b81d45e786d72128d099e64220e8e05d18
RYU <kshykl9@gamil.com>
2023-09-13 20:31:45 +0900

    UPDATE: comment naming

--- models/comment.dao.js
+++ models/comment.dao.js
@@ -10,7 +10,7 @@ const checkThreadById = async (id) => {
 
 const createComments = async (body) => {
   const comment = await AppDataSource.query(
-    `INSERT INTO thread_comments (thread_id, user_id, content) VALUES ('${body.threadId}','${body.userId}','${body.content}')`
+    `INSERT INTO thread_comments (thread_id, user_id, content) VALUES ('${body.postId}','${body.userId}','${body.content}')`
   );
 
   return comment;
--- services/comment.service.js
+++ services/comment.service.js
@@ -2,13 +2,13 @@ const { commentDao } = require("../models");
 const { throwError } = require("../utils/throwError");
 
 const createComment = async (body) => {
-  const { userId, threadId } = body;
+  const { userId, postId } = body;
   const user = await commentDao.checkUserById(userId);
   if (!user) {
     throwError(401, "USER_NOT_FOUND");
   }
 
-  const thread = await commentDao.checkThreadById(threadId);
+  const thread = await commentDao.checkThreadById(postId);
 
   //thread_does_not_exist
   if (!thread) {
