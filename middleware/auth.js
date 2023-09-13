358815b5f3c9754cfb90e023437dfc27d5888038
JunwooPark <wnsdn1994@gmail.com>
2023-09-13 18:32:48 +0900

    UPDATE : validateToken 에러 해결

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,13 +9,13 @@ const validateToken = async (req, res, next) => {
     const token = req.headers.authorization;
     const { id } = jwt.verify(token, AUTH_TOKEN_SALT); // 암호화된 토큰을 복호화 합니다.
 
-    const foundUser = await UserService.findUser({ id });
+    const foundUser = await UserService.findUser(id);
 
     if (!foundUser) {
       // 이 토큰을 가진 유저가 데이터베이스에 없으면 404 에러를 냅니다.
       throwError(404, "USER_NOT_FOUND");
     }
-    req.foundUser = foundUser; // request 객체에 새로운 키값에 찾아진 유저의 정보를 할당하고
+    req.body.user_id = id; // request 객체에 새로운 키값에 찾아진 유저의 정보를 할당하고
     next(); // next() 함수로 다음 미들웨어로 맥락(context)를 연결합니다.
   } catch (error) {
     next(error);
